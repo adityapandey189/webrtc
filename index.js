36ae3dbf615d306911530b427536f055513fefb1
adityapandey189 <aditya.pandey@eulermotors.com>
2024-10-11 02:39:54 +0530

    Added changes index.js file

--- index.js
+++ index.js
@@ -6,26 +6,23 @@ var http = require('http');
 var socketIO = require('socket.io');
 const port = process.env.PORT || 8000;
 
-var fileServer = new nodeStatic.Server();
-var app = http.createServer(function(req, res) {
-  // Use a callback for error handling in fileServer.serve
-  fileServer.serve(req, res, function(err, result) {
-    if (err) {
-      // Log the error and respond with an error message if headers haven't been sent
-      console.error('Error serving ' + req.url + ' - ' + err.message);
+var fileServer = new(nodeStatic.Server)();
 
-      // Check if headers have already been sent
-      if (!res.headersSent) {
-        res.writeHead(err.status || 500, { 'Content-Type': 'text/plain' });
-        res.end('Error serving ' + req.url + ' - ' + err.message);
-      }
+var app = http.createServer(function(req, res) {
+  // Serve files and handle errors properly
+  fileServer.serve(req, res, function(err) {
+    if (err) {
+      console.error('Error serving ' + req.url + ' - ' + err.message);
+      res.writeHead(err.status, err.headers);
+      res.end();
     }
   });
 }).listen(port);
 
 var io = socketIO.listen(app);
-
 io.sockets.on('connection', function(socket) {
+
+  // Convenience function to log server messages on the client
   function log() {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
@@ -34,11 +31,13 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     log('Client said: ', message);
+    // For a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
+
     var clientsInRoom = io.sockets.adapter.rooms.get(room);
     var numClients = clientsInRoom ? clientsInRoom.size : 0;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
@@ -47,13 +46,14 @@ io.sockets.on('connection', function(socket) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
+
     } else if (numClients === 1) {
-      socket.join(room);
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
+      socket.join(room);
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
-    } else {
+    } else { // max two clients
       socket.emit('full', room);
     }
   });
@@ -72,4 +72,5 @@ io.sockets.on('connection', function(socket) {
   socket.on('bye', function() {
     console.log('received bye');
   });
+
 });
