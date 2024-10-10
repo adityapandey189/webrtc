611dc644ec9bc918cdbc9e0c3a39bf08a240c688
adityapandey189 <aditya.pandey@eulermotors.com>
2024-10-11 02:24:01 +0530

    Added changes index.js file

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 'use strict';
 
 var os = require('os');
-var express = require('express');
+var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 const port = process.env.PORT || 8000;
 
-var app = express();
-var server = http.createServer(app);
+var fileServer = new(nodeStatic.Server)();
+var app = http.createServer(function(req, res) {
+  fileServer.serve(req, res);
+}).listen(port);
 
-// Serve static files from the 'public' directory
-app.use(express.static(__dirname + '/public'));
-
-// Initialize Socket.IO on the server
-var io = socketIO.listen(server);
+var io = socketIO.listen(app);
 
 io.sockets.on('connection', function(socket) {
 
-  // convenience function to log server messages on the client
+  // Convenience function to log server messages on the client
   function log() {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
@@ -26,15 +24,15 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     log('Client said: ', message);
-    // for a real app, this would be room-only (not broadcast)
+    // Broadcasting the message to everyone except the sender
     socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
 
-    var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    var clientsInRoom = io.sockets.adapter.rooms.get(room);
+    var numClients = clientsInRoom ? clientsInRoom.size : 0;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients === 0) {
@@ -48,7 +46,8 @@ io.sockets.on('connection', function(socket) {
       socket.join(room);
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
-    } else { // max two clients
+    } else { 
+      // Maximum two clients in a room
       socket.emit('full', room);
     }
   });
@@ -57,7 +56,7 @@ io.sockets.on('connection', function(socket) {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
       ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1' && details.address !== '10.10.11.176') {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
           socket.emit('ipaddr', details.address);
         }
       });
@@ -67,9 +66,15 @@ io.sockets.on('connection', function(socket) {
   socket.on('bye', function(){
     console.log('received bye');
   });
+
 });
 
-// Start the server
-server.listen(port, function() {
-  console.log('Server is running on port ' + port);
+// Centralized error handling middleware for the Node.js server
+app.on('error', (err, req, res, next) => {
+  if (res.headersSent) {
+    console.error('Headers were already sent, cannot modify response!', err);
+    return;
+  }
+
+  res.status(500).send('Internal Server Error');
 });
