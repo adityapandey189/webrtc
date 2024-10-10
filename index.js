998c61cdd0b5a0e47c53902f49df3ee52efdb911
adityapandey189 <aditya.pandey@eulermotors.com>
2024-10-10 16:57:01 +0530

    Added changes in the all  file

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const nodeStatic = require('node-static');
-const cors = require('cors');
+'use strict';
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const fileServer = new nodeStatic.Server();
+var os = require('os');
+var nodeStatic = require('node-static');
+var http = require('http');
+var socketIO = require('socket.io');
+const port = process.env.PORT || 8000; // Use PORT from environment
 
-app.use(cors());
-
-// Enable CORS for all requests
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Replace * with your client URL if needed
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+var fileServer = new(nodeStatic.Server)();
+var app = http.createServer(function (req, res) {
+  fileServer.serve(req, res);
+}).listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-io.on('connection', function(socket) {
-  // Your socket event handlers...
-  socket.on('message', function(message) {
-    // Handle incoming messages...
+var io = socketIO(app);
+io.sockets.on('connection', function (socket) {
+
+  // convenience function to log server messages on the client
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('message', function (message) {
+    log('Client said: ', message);
+    socket.broadcast.emit('message', message);
   });
 
-  socket.on('create or join', function(room) {
-    // Handle room creation/joining...
+  socket.on('create or join', function (room) {
+    log('Received request to create or join room ' + room);
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 0) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+    } else if (numClients === 1) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
   });
 
-  socket.on('bye', function() {
+  socket.on('bye', function () {
     console.log('received bye');
   });
-});
-
-// Vercel handles the port automatically, so you don't need to specify it.
-// Simply listen on the server.
-server.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running');
 });
--- js/main.js
+++ js/main.js
@@ -5,60 +5,37 @@ var isInitiator = false;
 var isStarted = false;
 var localStream;
 var pcs = {}; // Store multiple PeerConnections
-var remoteStreams = {}; // Store remote streams
-var turnReady;
-
-var pcConfig = {
-  'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
-  }]
-};
-
-var sdpConstraints = {
-  'mandatory': {
-    'OfferToReceiveAudio': true,
-    'OfferToReceiveVideo': true
-  }
-};
-
 var room = 'foo';
-var socket = io.connect('https://webrtc-two-eta.vercel.app/');
+var socket = io.connect();
 
+// Check if the room is defined and attempt to join
 if (room !== '') {
   socket.emit('create or join', room);
   console.log('Attempted to create or join room', room);
 }
 
-socket.on('created', function(room) {
+socket.on('created', function (room) {
   console.log('Created room ' + room);
   isInitiator = true;
 });
 
-socket.on('full', function(room) {
+socket.on('full', function (room) {
   console.log('Room ' + room + ' is full');
 });
 
-socket.on('join', function(room) {
+socket.on('join', function (room) {
   console.log('Another peer made a request to join room ' + room);
   console.log('This peer is the initiator of room ' + room + '!');
   isChannelReady = true;
 });
 
-socket.on('joined', function(room) {
+socket.on('joined', function (room) {
   console.log('joined: ' + room);
   isChannelReady = true;
 });
 
-socket.on('log', function(array) {
-  console.log.apply(console, array);
-});
-
-function sendMessage(message) {
-  console.log('Client sending message: ', message);
-  socket.emit('message', message);
-}
-
-socket.on('message', function(message) {
+// Handling messages from the server
+socket.on('message', function (message) {
   console.log('Client received message:', message);
   if (message === 'got user media') {
     maybeStart();
@@ -66,39 +43,35 @@ socket.on('message', function(message) {
     if (!isInitiator && !isStarted) {
       maybeStart();
     }
-    var peerId = message.peerId; // Get the peerId
-    pcs[peerId].setRemoteDescription(new RTCSessionDescription(message));
-    doAnswer(peerId);
+    pcs[message.peerId].setRemoteDescription(new RTCSessionDescription(message));
+    doAnswer(message.peerId);
   } else if (message.type === 'answer' && isStarted) {
-    var peerId = message.peerId; // Get the peerId
-    pcs[peerId].setRemoteDescription(new RTCSessionDescription(message));
+    pcs[message.peerId].setRemoteDescription(new RTCSessionDescription(message));
   } else if (message.type === 'candidate' && isStarted) {
     var candidate = new RTCIceCandidate({
       sdpMLineIndex: message.label,
       candidate: message.candidate
     });
-    pcs[message.peerId].addIceCandidate(candidate); // Add candidate to the correct PeerConnection
+    pcs[message.peerId].addIceCandidate(candidate);
   } else if (message === 'bye' && isStarted) {
     handleRemoteHangup();
   }
 });
 
-var localVideo = document.querySelector('#localVideo');
-var remoteVideos = {}; // Store references to remote video elements
-
+// Getting local media stream
 navigator.mediaDevices.getUserMedia({
   audio: true,
   video: true
 })
 .then(gotStream)
-.catch(function(e) {
+.catch(function (e) {
   alert('getUserMedia() error: ' + e.name);
 });
 
 function gotStream(stream) {
   console.log('Adding local stream.');
   localStream = stream;
-  localVideo.srcObject = stream;
+  document.querySelector('#localVideo').srcObject = stream;
   sendMessage('got user media');
   if (isInitiator) {
     maybeStart();
@@ -107,25 +80,27 @@ function gotStream(stream) {
 
 function maybeStart() {
   console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
-  if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
-    console.log('>>>>>> creating peer connection');
-    createPeerConnection(); // Call for each peer
-    pcs[socket.id].addStream(localStream); // Add local stream to the connection
+  if (!isStarted && localStream && isChannelReady) {
+    createPeerConnection(); 
+    pcs[socket.id].addStream(localStream); 
     isStarted = true;
-    console.log('isInitiator', isInitiator);
     if (isInitiator) {
-      doCall(socket.id); // Call for each peer
+      doCall(socket.id);
     }
   }
 }
 
-function createPeerConnection(peerId) {
+function createPeerConnection() {
   try {
-    pcs[peerId] = new RTCPeerConnection(pcConfig);
-    pcs[peerId].onicecandidate = handleIceCandidate;
-    pcs[peerId].onaddstream = handleRemoteStreamAdded;
-    pcs[peerId].onremovestream = handleRemoteStreamRemoved;
-    console.log('Created RTCPeerConnnection for peer ID: ' + peerId);
+    pcs[socket.id] = new RTCPeerConnection({
+      'iceServers': [{
+        'urls': 'stun:stun.l.google.com:19302'
+      }]
+    });
+    pcs[socket.id].onicecandidate = handleIceCandidate;
+    pcs[socket.id].onaddstream = handleRemoteStreamAdded;
+    pcs[socket.id].onremovestream = handleRemoteStreamRemoved;
+    console.log('Created RTCPeerConnnection');
   } catch (e) {
     console.log('Failed to create PeerConnection, exception: ' + e.message);
     alert('Cannot create RTCPeerConnection object.');
@@ -134,54 +109,44 @@ function createPeerConnection(peerId) {
 }
 
 function handleIceCandidate(event) {
-  console.log('icecandidate event: ', event);
   if (event.candidate) {
     sendMessage({
       type: 'candidate',
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
       candidate: event.candidate.candidate,
-      peerId: this.peerId // Add peerId here
+      peerId: socket.id 
     });
-  } else {
-    console.log('End of candidates.');
   }
 }
 
 function doCall(peerId) {
-  console.log('Sending offer to peer ' + peerId);
   pcs[peerId].createOffer(setLocalAndSendMessage.bind(null, peerId), handleCreateOfferError);
 }
 
 function doAnswer(peerId) {
-  console.log('Sending answer to peer ' + peerId);
-  pcs[peerId].createAnswer().then(
-    setLocalAndSendMessage.bind(null, peerId),
-    onCreateSessionDescriptionError
-  );
+  pcs[peerId].createAnswer().then(setLocalAndSendMessage.bind(null, peerId), onCreateSessionDescriptionError);
 }
 
 function setLocalAndSendMessage(peerId, sessionDescription) {
   pcs[peerId].setLocalDescription(sessionDescription);
-  console.log('setLocalAndSendMessage sending message', sessionDescription);
-  sendMessage({
-    ...sessionDescription,
-    peerId: peerId // Include peerId
-  });
+  sendMessage({ ...sessionDescription, peerId: peerId });
 }
 
 function handleRemoteStreamAdded(event) {
-  console.log('Remote stream added.');
-  var remoteVideo = document.createElement('video'); // Create a new video element
+  var remoteVideo = document.createElement('video');
   remoteVideo.srcObject = event.stream;
-  document.body.appendChild(remoteVideo); // Append the video to the body (or any container)
-  remoteStreams[event.stream.id] = event.stream; // Store the stream reference
+  document.body.appendChild(remoteVideo);
 }
 
 function handleRemoteStreamRemoved(event) {
   console.log('Remote stream removed. Event: ', event);
 }
 
-window.onbeforeunload = function() {
+function sendMessage(message) {
+  socket.emit('message', message);
+}
+
+window.onbeforeunload = function () {
   sendMessage('bye');
 };
